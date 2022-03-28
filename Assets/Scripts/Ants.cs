1c3b726f1f7e350e2c5194500167cfbbf73b34fe
Mathias Depoorter <mathias.depoorter@limayrac.fr>
2022-03-28 23:49:35 +0200

    Update movement + Create Animation
    Correct bug when ants was stuck on wall.
    
    Create an animation for ants deplacement

--- Assets/Scripts/Ants.cs
+++ Assets/Scripts/Ants.cs
@@ -7,22 +7,29 @@ public class Ants : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    private bool test = true;
-
     // Start is called before the first frame update
     void Start()
     {
-        //int angle = Random.Range(0, 360);
-        //transform.Rotate(Vector3.forward * angle);
+        int angle = Random.Range(0, 360);
+        transform.Rotate(Vector3.forward * angle);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+        
     }
 
+    //Change Angle in collision
     void OnCollisionEnter2D(Collision2D col) {
-        transform.Rotate(Vector3.forward * Random.Range(92, 124));
+        //transform.Rotate(Vector3.forward * Random.Range(92, 124));
+    }
+
+    //Change Angle in collision and when Ants is stuck
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        transform.Rotate(Vector3.forward * Random.Range(60, 170));
     }
 }
