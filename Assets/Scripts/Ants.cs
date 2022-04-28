3d398ba995b00013f5e7e079e6874a5711e981e4
Mathias Depoorter <mathias.depoorter@limayrac.fr>
2022-04-28 10:53:53 +0200

    Move Ants

--- Assets/Scripts/Ants.cs
+++ Assets/Scripts/Ants.cs
@@ -7,29 +7,67 @@ public class Ants : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    bool proxiFood = false;
+
     // Start is called before the first frame update
     void Start()
     {
         int angle = Random.Range(0, 360);
         transform.Rotate(Vector3.forward * angle);
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.up * speed * Time.deltaTime);
-        
-    }
+        GameObject ojectFood;
 
-    //Change Angle in collision
-    void OnCollisionEnter2D(Collision2D col) {
-        //transform.Rotate(Vector3.forward * Random.Range(92, 124));
+        ojectFood = searchFood();
+        if (proxiFood == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, ojectFood.transform.position, Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
     }
 
     //Change Angle in collision and when Ants is stuck
     void OnCollisionStay2D(Collision2D collision)
     {
-        transform.Rotate(Vector3.forward * Random.Range(60, 170));
+        switch (collision.gameObject.tag)
+        {
+            //Contact with food
+            case "Food":
+                collision.gameObject.GetComponent<Food>().quantity--;
+                Debug.Log(collision.gameObject.GetComponent<Food>().quantity);
+                if (collision.gameObject.GetComponent<Food>().quantity <= 0)
+                {
+                    Destroy(collision.gameObject);
+                    proxiFood = false;
+                    transform.Translate(Vector2.up * speed * Time.deltaTime);
+                }
+                break;
+            default:
+                transform.Rotate(Vector3.forward * Random.Range(60, 170));
+                break;
+        }
+    }
+
+    //search food in radius of 1
+    GameObject searchFood()
+    {
+        GameObject gameObject = null;
+        GameObject[] foods = GameObject.FindGameObjectsWithTag("Food");
+        foreach (var food in foods)
+        {
+            if (Vector3.Distance(transform.position, food.transform.position) < 2)
+            {
+                //Debug.Log(Vector3.Distance(transform.position, food.transform.position));
+                proxiFood = true;
+                gameObject =  food;
+            }
+        }
+        return gameObject;
     }
 }
